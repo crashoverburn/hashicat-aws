6b8509f1b0cda45c2c4b2c0d9363ca690edf6b90
Maiquel Rinco <maiquelrinco@hotmail.com>
2022-10-20 18:56:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,12 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/Zurich-Chile/s3-bucket/aws"
   version = "2.8.0"
 
-  prefix = "cl" 
   bucket_prefix = prefix  
   bucket = "Gaurav"
   acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
 }
